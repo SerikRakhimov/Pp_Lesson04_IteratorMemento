bbbe6ccbe3fea6a0f6652fab3a3d6a7e1eda479a
RSB <s_astana@mail.ru>
2019-07-25 04:03:37 +0600

    first commit

---
+++ Pp_Iterator/Pp_Iterator/Pp_Iterator/Program.cs
@@ -0,0 +1,158 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Collections;
+
+// Iterator(Итератор)
+// Дан список слов.
+// Необходимо создать итератор,
+// печатающий сперва все четные элементы, затем все нечетные.
+
+namespace Pp_Iterator
+{
+    abstract class Iterator : IEnumerator
+    {
+        object IEnumerator.Current => Current();
+
+        // Возвращает ключ текущего элемента
+        public abstract int Key();
+
+        // Возвращает текущий элемент.
+        public abstract object Current();
+
+        // Переходит к следующему элементу.
+        public abstract bool MoveNext();
+
+        // Перематывает Итератор к первому элементу.
+        public abstract void Reset();
+    }
+
+    abstract class IteratorAggregate : IEnumerable
+    {
+        // Возвращает Iterator или другой IteratorAggregate для реализующего
+        // объекта.
+        public abstract IEnumerator GetEnumerator();
+    }
+
+    // Конкретные Итераторы реализуют различные алгоритмы обхода. Эти классы
+    // постоянно хранят текущее положение обхода.
+    class AlphabeticalOrderIterator : Iterator
+    {
+        private WordsCollection _collection;
+
+        // Хранит текущее положение обхода. У итератора может быть множество
+        // других полей для хранения состояния итерации, особенно когда он
+        // должен работать с определённым типом коллекции.
+        private int _position = -1;
+
+        private bool _chetnye = true;
+
+        public AlphabeticalOrderIterator(WordsCollection collection)
+        {
+            this._collection = collection;
+            this._position = -2;
+        }
+
+        public override object Current()
+        {
+            return this._collection.getItems()[_position];
+        }
+
+        public override int Key()
+        {
+            return this._position;
+        }
+
+        public override bool MoveNext()
+        {
+            int updatedPosition = this._position + 2;
+
+            if (updatedPosition >= 0 && updatedPosition < this._collection.getItems().Count)
+            {
+                this._position = updatedPosition;
+                return true;
+            }
+            else
+            {
+                if (this._chetnye == true)
+                {
+                    this._chetnye = false;
+
+                    updatedPosition = 1;
+
+                    if (updatedPosition >= 0 && updatedPosition < this._collection.getItems().Count)
+                    {
+                        this._position = updatedPosition;
+                        return true;
+                    }
+                    else
+                    {
+                        return false;
+                    }
+                }
+                else
+                {
+                    return false;
+                }
+            }
+        }
+
+        public override void Reset()
+        {
+            this._position = 0;
+        }
+    }
+
+    // Конкретные Коллекции предоставляют один или несколько методов для
+    // получения новых экземпляров итератора, совместимых с классом коллекции.
+    class WordsCollection : IteratorAggregate
+    {
+        List<string> _collection = new List<string>();
+
+        public List<string> getItems()
+        {
+            return _collection;
+        }
+
+        public void AddItem(string item)
+        {
+            this._collection.Add(item);
+        }
+
+        public override IEnumerator GetEnumerator()
+        {
+            return new AlphabeticalOrderIterator(this);
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Клиентский код может знать или не знать о Конкретном
+            // Итераторе или классах Коллекций, в зависимости от уровня
+            // косвенности, который вы хотите сохранить в своей программе.
+            var collection = new WordsCollection();
+            collection.AddItem("Слива0");
+            collection.AddItem("Персик1");
+            collection.AddItem("Вишня2");
+            collection.AddItem("Арбуз3");
+            collection.AddItem("Дыня4");
+            collection.AddItem("Виноград5");
+            collection.AddItem("Яблоко6");
+            collection.AddItem("Груша7");
+            collection.AddItem("Апельсин8");
+
+            Console.WriteLine("Сначала четные элементы, затем нечетные:");
+
+            foreach (var element in collection)
+            {
+                Console.WriteLine(element);
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
---
+++ Pp_Memento/Pp_Memento/Pp_Memento/Program.cs
@@ -0,0 +1,193 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+// Memento (Снимок):
+// Игровой персонаж совершает выстрелы,
+// необходимо сохранять состояния игрока после серии выстрелов.
+
+namespace Pp_Memento
+{
+    // Создатель содержит некоторое важное состояние, которое может со
+    // временем меняться. Он также объявляет метод сохранения состояния внутри
+    // снимка и метод восстановления состояния из него.
+    class Originator
+    {
+        // Для удобства состояние создателя хранится внутри одной
+        // переменной.
+        private string _state;
+
+        public Originator(string state)
+        {
+            this._state = state;
+            Console.WriteLine("Первоначальное состояние: " + state);
+        }
+
+        // Бизнес-логика Создателя может повлиять на его внутреннее
+        // состояние. Поэтому клиент должен выполнить резервное копирование
+        // состояния с помощью метода save перед запуском методов бизнес-логики.
+        public void DoSomething()
+        {
+            this._state = this.GenerateRandomString();
+            Console.WriteLine($"Персонаж: {_state}");
+        }
+
+        private string GenerateRandomString()
+        {
+            string result = "Выстрел: " + (new Random().Next(0, 30) + 10).ToString();
+
+            return result;
+        }
+
+        // Сохраняет текущее состояние внутри снимка.
+        public IMemento Save()
+        {
+            return new ConcreteMemento(this._state);
+        }
+
+        // Восстанавливает состояние Создателя из объекта снимка.
+        public void Restore(IMemento memento)
+        {
+            if (!(memento is ConcreteMemento))
+            {
+                throw new Exception("Неизвестный снимок " + memento.ToString());
+            }
+
+            this._state = memento.GetState();
+            Console.WriteLine($"Текущее состояние: {_state}");
+        }
+    }
+
+    // Интерфейс Снимка предоставляет способ извлечения метаданных снимка,
+    // таких как дата создания или название. Однако он не раскрывает состояние
+    // Создателя.
+    public interface IMemento
+    {
+        string GetName();
+
+        string GetState();
+
+        DateTime GetDate();
+    }
+
+    // Конкретный снимок содержит инфраструктуру для хранения состояния
+    // Создателя.
+    class ConcreteMemento : IMemento
+    {
+        private string _state;
+
+        private DateTime _date;
+
+        public ConcreteMemento(string state)
+        {
+            this._state = state;
+            this._date = DateTime.Now;
+        }
+
+        // Создатель использует этот метод, когда восстанавливает своё
+        // состояние.
+        public string GetState()
+        {
+            return this._state;
+        }
+
+        // Остальные методы используются Опекуном для отображения
+        // метаданных.
+        public string GetName()
+        {
+            return $"{this._date} / ({this._state.Substring(0, 11)})...";
+        }
+
+        public DateTime GetDate()
+        {
+            return this._date;
+        }
+    }
+
+    // Опекун не зависит от класса Конкретного Снимка. Таким образом, он не
+    // имеет доступа к состоянию создателя, хранящемуся внутри снимка. Он
+    // работает со всеми снимками через базовый интерфейс Снимка.
+    class Caretaker
+    {
+        private List<IMemento> _mementos = new List<IMemento>();
+
+        private Originator _originator = null;
+
+        public Caretaker(Originator originator)
+        {
+            this._originator = originator;
+        }
+
+        public void Backup()
+        {
+            Console.WriteLine("Сохранение снимка...\n");
+            Thread.Sleep(1000);
+            this._mementos.Add(this._originator.Save());
+        }
+
+        public void Undo()
+        {
+            if (this._mementos.Count == 0)
+            {
+                return;
+            }
+
+            var memento = this._mementos.Last();
+            this._mementos.Remove(memento);
+
+            Console.WriteLine("\nВосстановлено состояние: " + memento.GetName());
+
+            try
+            {
+                this._originator.Restore(memento);
+            }
+            catch (Exception)
+            {
+                this.Undo();
+            }
+        }
+
+        public void ShowHistory()
+        {
+            Console.WriteLine("Список сохраненных снимков:");
+
+            foreach (var memento in this._mementos)
+            {
+                Console.WriteLine(memento.GetName());
+            }
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Клиентский код.
+            Originator originator = new Originator("Подготовка ");
+            Caretaker caretaker = new Caretaker(originator);
+            caretaker.Backup();
+
+            originator.DoSomething();
+            caretaker.Backup();
+
+            originator.DoSomething();
+            caretaker.Backup();
+
+            originator.DoSomething();
+            caretaker.Backup();
+
+            Console.WriteLine();
+            caretaker.ShowHistory();
+
+            caretaker.Undo();
+
+            caretaker.Undo();
+
+            Console.ReadLine();
+        }
+    }
+
+}
